973b5abc26f294670a9c1cf0f429888a9debee0f
cestesud <cestes@udel.edu>
2021-03-12 09:03:08 -0500

    This change is in Branch 2.

--- test.php
+++ test.php
@@ -1,3 +1,3 @@
 <?php
-echo "This is a test of my new git repository. Re-adding the sentence because I don't know why it was removed. I'm now adding a change that I think will update my branch.";
+echo "This is a test of my new git repository. Re-adding the sentence because I don't know why it was removed. This sentence is in Branch 2.";
  ?>
